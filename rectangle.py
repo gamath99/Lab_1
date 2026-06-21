01a1e8f2dec6ae89059af50baf82224967b2f36b
gamath99 <gamamathurin1@gmail.com>
2026-06-21 16:47:44 -0400

    final seond file to calculate area and perimeter of a rectangle

---
+++ rectangle.py
@@ -0,0 +1,11 @@
+#creation of the file that will help to calculate the area and the perimeter of a rectangle 
+
+# Assign a function statement to calculate the area of a rectangle by using width and height as arguments. 
+def calc_area(width,height):
+    #formula to calculate the are of a rectangle is width multiply by height 
+    return width * height
+
+#Assign a function statement to calculate the perimeter of a rectangle 
+def calc_perimeter(width, height):
+    #formula to calculate the area of a rectangle is the sum of width and height multiply by 2
+    return 2*(width + height)
