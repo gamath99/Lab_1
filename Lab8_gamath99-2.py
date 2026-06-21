260a6b337a51677db907533e4d837c23b6c5ef48
gamath99 <gamamathurin1@gmail.com>
2026-06-21 18:51:59 -0400

    Assign function to capture the correct value for the program

--- Lab8_gamath99-2.py
+++ Lab8_gamath99-2.py
@@ -4,3 +4,32 @@
 
 import circle as c
 import rectangle as r
+
+# Assign a funtion statement that can select the correct value for the loop that will execute
+# Filter positive value to execute 
+
+def capture_positive_number(initial):
+    # The while loop will repeat until it capture the correct input value under conditions 
+    While True: 
+
+        value = input(initial)
+    # Use of the method .isdigit to only the digit and ignore the other value 
+        if value.isdigit():
+            value = float(value)
+            if value > 0: 
+                return value
+            else:
+                print("Error: Please enter a positive number.")
+        else: 
+            print("Error : Please enter a valid number.")
+
+
+
+    
+    
+    
+
+              
+    
+
+
